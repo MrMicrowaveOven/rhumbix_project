dd561c7dc4248fdb2ce289b0f916757b6d5a931a
mrmicrowaveoven <benj@zagorski.com>
2020-11-19 00:40:07 -0800

    Added hover highlight

--- src/components/AutofillOption.js
+++ src/components/AutofillOption.js
@@ -1,7 +1,12 @@
 function AutofillOption(props) {
   const {value} = props;
+
+  const handleClick = (e) => {
+    const text = e.target.innerText
+  }
+
   return (
-    <div className="autofill-option" key={value}>
+    <div className="autofill-option" key={value} onClick={handleClick}>
       {value}
     </div>
   );
