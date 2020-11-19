886a1035bca4762f4efa6a84a029a4965396a693
mrmicrowaveoven <benj@zagorski.com>
2020-11-18 23:50:39 -0800

    Moved getGiphy logic to Gif component

--- src/App.js
+++ src/App.js
@@ -3,34 +3,16 @@ import Gif from './components/Gif.js'
 import { useState } from 'react';
 
 function App() {
-  const getGiphy = (keyword) => {
-    const giphyFetchUrl = "https://api.giphy.com/v1/gifs/search?q=" + keyword + "&api_key=DLCVuTK6KZExOS7JoMq82bi5MaI6EbWO&limit=1"
-    fetch(giphyFetchUrl)
-    .then(response => {
-      return response.json();
-    })
-    .then(jsonResponse => {
-      console.log(jsonResponse)
-      const url = jsonResponse.data[0].images.original.url
-      console.log(url)
-      setGifUrl(url)
-    });
-  }
-
   const handleChange = (e) => {
     setInputValue(e.target.value)
   }
 
-  const setGif = () => {
-    setGifUrl(inputValue)
-  }
-
   const handleClick = () => {
-    getGiphy(inputValue)
+    setKeyword(inputValue)
   }
 
   const [inputValue, setInputValue] = useState('');
-  const [gifUrl, setGifUrl] = useState('')
+  const [keyword, setKeyword] = useState('')
 
   return (
     <div className="App">
@@ -38,7 +20,7 @@ function App() {
       <input type="text" id="gifKeyword" name="" onChange={handleChange} />
       <button type="button" name="button" onClick={handleClick}>Fetch me a Gif!</button>
 
-      <Gif url={gifUrl}/>
+      <Gif keyword={keyword}/>
     </div>
   );
 }
--- src/components/Gif.js
+++ src/components/Gif.js
@@ -1,8 +1,32 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
 function Gif(props) {
+
+  useEffect(() => {
+    const {keyword} = props;
+    if (keyword !== '') { getGiphy(keyword) }
+  })
+
+  const getGiphy = (keyword) => {
+    const giphyFetchUrl = "https://api.giphy.com/v1/gifs/search?q=" + keyword + "&api_key=DLCVuTK6KZExOS7JoMq82bi5MaI6EbWO&limit=1"
+    fetch(giphyFetchUrl)
+    .then(response => {
+      return response.json();
+    })
+    .then(jsonResponse => {
+      console.log(jsonResponse)
+      const url = jsonResponse.data[0].images.original.url
+      console.log(url)
+      setGifUrl(url)
+    });
+  }
+
+  const {keyword} = props
+  const [gifUrl, setGifUrl] = useState('')
+
   return (
     <div className="gif">
-      <img src={props.url} />
+      <img src={gifUrl} />
     </div>
   );
 }
